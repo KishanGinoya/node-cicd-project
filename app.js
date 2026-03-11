2eb6cb16919a16f5cd4d44ec326c36dcd53a7126
Kishan Ginoya <kishanginoya1284@gmail.com>
2026-03-11 22:40:53 +0530

    Add Version 2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express=require('express')
 const app=express()
 
 app.get('/',(req,res)=>{
-    res.send('CI/CD Pipline Working')
+    res.send('Version 2 deployed 🚀')
 })
 
 app.listen(3000,()=>{
